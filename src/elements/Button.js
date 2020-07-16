12aff3826b792544c100927c319b3a6f2236c335
sugeng.wanantara <sugeng.wanantara@gmail.com>
2020-07-16 14:09:36 +0700

    updater akhir

--- src/elements/Button.js
+++ src/elements/Button.js
@@ -2,15 +2,17 @@ import React from "react";
 import propTypes from "prop-types";
 
 export default function Button(props) {
-  const className = ["button"];
-  if (props.isPrimary) className.push("button-primary");
-  if (props.isWideMobile) className.push("button-wide-mobil");
-  if (props.isSmall) className.push("button-sm");
-  return <button className={className.join(" ")}>{props.children}</button>;
+    const className = ["button"];
+    if (props.isPrimary) className.push("button-primary");
+    if (props.isWideMobile) className.push("button-wide-mobil");
+    if (props.isSmall) className.push("button-sm");
+    if (props.isBlock) className.push("button-block");
+    return <button className={className.join(" ")}>{props.children}</button>;
 }
 
 Button.propTypes = {
-  isPrimary: propTypes.string,
-  isWideMobile: propTypes.string,
-  isSmall: propTypes.string,
+    isPrimary: propTypes.bool,
+    isWideMobile: propTypes.bool,
+    isSmall: propTypes.bool,
+    isBlock: propTypes.bool,
 };
--- src/scenes/Homepage.js
+++ src/scenes/Homepage.js
@@ -1,12 +1,128 @@
 import React from "react";
+import Fade from "react-reveal/Fade";
 
 import Header from "parts/Header";
 import Clients from "parts/Clients";
+import Footer from "parts/Footer";
 import Section from "elements/Section";
+import Feature from "parts/Feature";
+import Card from "elements/Card";
+import List from "elements/Lists";
+import Button from "elements/Button";
+
 import HeroImage from "assets/images/hero-image.png";
-import Fade from "react-reveal/Fade";
+import FeatureTile01 from "assets/images/feature-tile-icon-01.svg";
+import FeatureTile02 from "assets/images/feature-tile-icon-02.svg";
+import FeatureTile03 from "assets/images/feature-tile-icon-03.svg";
 
 export default function Homepage() {
+    const features = [
+        {
+            imgSrc: FeatureTile01,
+            imgAlt: "Feature tile icon 01",
+            title: "Join Itgenic",
+            description:
+                "A pseudo-Latin text used in web design, layout, and printing in place of things to emphasise design.",
+        },
+        {
+            imgSrc: FeatureTile02,
+            imgAlt: "Feature tile icon 02",
+            title: "Join the system",
+            description:
+                "A pseudo-Latin text used in web design, layout, and printing in place of things to emphasise design.",
+        },
+        {
+            imgSrc: FeatureTile03,
+            imgAlt: "Feature tile icon 03",
+            title: "Join the system",
+            description:
+                "A pseudo-Latin text used in web design, layout, and printing in place of things to emphasise design.",
+        },
+    ];
+
+    const listPricing = [
+        {
+            price: 27,
+            currencySymbol: "$",
+            description: "Lorem ipsum is a common text",
+            features: [
+                {
+                    isChecked: true,
+                    content: "Excepteur sint occaecat velit",
+                },
+                {
+                    isChecked: true,
+                    content: "Excepteur sint occaecat velit",
+                },
+                {
+                    isChecked: true,
+                    content: "Excepteur sint occaecat velit",
+                },
+                {
+                    isChecked: false,
+                    content: "Excepteur sint occaecat velit",
+                },
+                {
+                    isChecked: false,
+                    content: "Excepteur sint occaecat velit",
+                },
+            ],
+        },
+        {
+            price: 47,
+            currencySymbol: "$",
+            description: "Lorem ipsum is a common text",
+            features: [
+                {
+                    isChecked: true,
+                    content: "Excepteur sint occaecat velit",
+                },
+                {
+                    isChecked: true,
+                    content: "Excepteur sint occaecat velit",
+                },
+                {
+                    isChecked: true,
+                    content: "Excepteur sint occaecat velit",
+                },
+                {
+                    isChecked: true,
+                    content: "Excepteur sint occaecat velit",
+                },
+                {
+                    isChecked: false,
+                    content: "Excepteur sint occaecat velit",
+                },
+            ],
+        },
+        {
+            price: 67,
+            currencySymbol: "$",
+            description: "Lorem ipsum is a common text",
+            features: [
+                {
+                    isChecked: true,
+                    content: "Excepteur sint occaecat velit",
+                },
+                {
+                    isChecked: true,
+                    content: "Excepteur sint occaecat velit",
+                },
+                {
+                    isChecked: true,
+                    content: "Excepteur sint occaecat velit",
+                },
+                {
+                    isChecked: true,
+                    content: "Excepteur sint occaecat velit",
+                },
+                {
+                    isChecked: true,
+                    content: "Excepteur sint occaecat velit",
+                },
+            ],
+        },
+    ];
     return (
         <div className="body-wrap">
             <Header></Header>
@@ -49,7 +165,81 @@ export default function Homepage() {
                     </div>
                 </Section>
                 <Clients></Clients>
+                <Section className="features-tiles">
+                    <div className="container">
+                        <div className="features-tiles-inner section-inner">
+                            <div className="tiles-wrap">
+                                {features.map((feature, index) => (
+                                    <Feature
+                                        key={index}
+                                        delayInMS={index * 500}
+                                        data={feature}
+                                    ></Feature>
+                                ))}
+                            </div>
+                        </div>
+                    </div>
+                </Section>
+                <Section className="pricing">
+                    <div className="container">
+                        <div className="pricing-inner section-inner has-top-divider">
+                            <div className="section-header center-content">
+                                <div className="container-xs">
+                                    <h2 className="mt-0 mb-16">
+                                        Simple, transarent pricing
+                                    </h2>
+                                    <p className="m-0">
+                                        Lorem ipsum is common placeholder text
+                                        used to demonstrate the graphic elements
+                                        of a document or visual presentation.
+                                    </p>
+                                </div>
+                            </div>
+                            <div className="tiles-wrap">
+                                {listPricing.map((list, index) => (
+                                    <Card hasShadow>
+                                        <div class="pricing-item-content">
+                                            <div class="pricing-item-header pb-24 mb-24">
+                                                <div class="pricing-item-price mb-4">
+                                                    <span class="pricing-item-price-currency h2">
+                                                        {list.currencySymbol}
+                                                    </span>
+                                                    <span
+                                                        class="pricing-item-price-amount h1 pricing-switchable"
+                                                        data-pricing-monthly="34"
+                                                        data-pricing-yearly="27"
+                                                    >
+                                                        {list.price}
+                                                    </span>
+                                                </div>
+                                                <div class="text-xs text-color-low">
+                                                    {list.description}
+                                                </div>
+                                            </div>
+                                            <div class="pricing-item-features mb-40">
+                                                <div class="pricing-item-features-title h6 text-xs text-color-high mb-24">
+                                                    What’s included
+                                                </div>
+                                                <List
+                                                    data={list.features}
+                                                    isSmall
+                                                    className="pricing-item-features-list mb-32"
+                                                ></List>
+                                            </div>
+                                        </div>
+                                        <div class="pricing-item-cta mb-8">
+                                            <Button isPrimary isBlock>
+                                                Start Free Trial
+                                            </Button>
+                                        </div>
+                                    </Card>
+                                ))}
+                            </div>
+                        </div>
+                    </div>
+                </Section>
             </main>
+            <Footer></Footer>
         </div>
     );
 }
